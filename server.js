8d946f32fab7004ad6e3f432debe275df8cef56e
Hezronz <hezronbahati143@gmail.com>
2025-07-17 15:33:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const app = express();
 const PORT = 3000;
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// Serve files from 'frontend' folder instead
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+// Send login page at root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'student-login.html'));
 
 app.use(cors());
 app.use(express.json());
