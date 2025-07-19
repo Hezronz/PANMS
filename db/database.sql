191d86e367fcebf64e94a5f631e9c65e3f0e62fc
Hezronz <hezronbahati143@gmail.com>
2025-07-19 12:36:20 +0300

    chnaging to correct database name, db.js

--- db/database.sql
+++ db/database.sql
@@ -1,4 +1,4 @@
-USE railway;
+USE panms_db;
 
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
--- db/test.sql
+++ db/test.sql
@@ -2,8 +2,16 @@ USE panms_db;
 
 INSERT INTO users (name, email, password, role)
 VALUES (
-  'Hez',
-  'hezron5669@gmail.com',
+  'Hezron',
+  'hezron569@gmail.com',
   '$2b$10$EJ6UQqf3QrjH7K1ZcE6gLeX9fQWyNBTayC.thn7aV3bmgbFjBZ7PK',
   'student'
 );
+
+DELETE FROM users WHERE email = 'admin@panms.com';
+
+INSERT INTO users (name, email, password, role)
+VALUES ('Site Admin2', 'admin123@gmail.com',
+        '$2b$10$cKSBHlLEtwETXwwrjhFWBOpFrU6jO8P.DXsbSfDGm.S2uAmPsNHyG',
+        'admin');
+
